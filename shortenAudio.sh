4da6fd633f99c8ca5093121bfddb00a1455d2647
Mesar Hameed <mesar.hameed@gmail.com>
2022-04-08 15:49:22 +0100

    Improvements: will now work in parallel, ffmpeg instead of lame

--- shortenAudio.sh
+++ shortenAudio.sh
@@ -5,7 +5,7 @@
 # Good range for most audio books, 0.5% 2.5%
 # Try the first chapter of each book to get the right value.
 # The higher value, without noticable word cuts, the more scilence is stripped.
-sensitivity=1.0%
+sensitivity=1%
 
 # After cutting out the pauses, how much faster should the recording be?
 # Reasonable range: 1.0 1.5
@@ -15,15 +15,12 @@ speedup=1.3
 
 . $(dirname $(readlink -f $0))/helper.sh
 
-checkexists lame
+checkexists ffmpeg
 checkexists mp3info
 checkexists sox
 checkexists calc
 
-in=/tmp/in.wav
-out=/tmp/out.wav
-
-outdir=./
+outdir=.
 
 if [ -n "$1" ] && [ "$1" == "--outdir" ] && [ -n "$2" ]; then
     outdir="$2"
@@ -34,21 +31,19 @@ fi
 # Process the files given on the commandline.
 while [ -n "$1" ]; do
     file="$1"
+    tmp1=$(mktemp -u /tmp/in-XXXXX.wav)
+    tmp2=$(mktemp -u /tmp/out-XXXXX.wav)
     echo "processing $file"
     lb=$(mp3info -p '%S' "$file")
-    lame --silent --decode $file $in
-    sox -q --norm $in $out silence 1 0.01 $sensitivity -1 0.1 $sensitivity \
+    ffmpeg -loglevel quiet -i $file $tmp1
+    sox $tmp1 $tmp2 norm -0.1
+    sox -q --norm $tmp2 $tmp1 silence 1 0.01 $sensitivity -1 0.1 $sensitivity \
         tempo -s $speedup
-    lame --silent -B 32 $out "${outdir}/trimmed.$file"
+    ffmpeg -loglevel quiet -i $tmp1 -y "${outdir}/trimmed.$file"
     la=$(mp3info -p '%S' "${outdir}/trimmed.$file")
     p=$(calc "round( ($la / $lb) * 100,2)")
     echo "trimmed version is ${p}% of original."
+    rm $tmp1 $tmp2
     shift
 done
 
-if [ -e "$in" ]; then
-    rm $in
-fi
-if [ -e "$out" ]; then
-    rm $out
-fi
